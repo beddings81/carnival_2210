cb5a8b1a24926392235217cddcdfee7b947f70c8
Braxton Eddings <beddings81@gmail.com>
2022-11-14 12:46:16 -0600

    Start iteration 4

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -2,9 +2,12 @@ class Carnival
   attr_reader :duration,
               :rides
 
+  attr_accessor :summary
+
   def initialize(duration)
     @duration = duration
     @rides = []
+    @summary = Hash.new
   end
 
   def add_ride(ride)
@@ -28,4 +31,18 @@ class Carnival
       ride.total_revenue
     end
   end
+
+  # def summary_hash
+  #   #visitor count
+  #   #revenue earned
+  #   #List of visitors and each visitor's favorite ride and how much total money a visitor spent
+  #   #List of rides and who rode each ride and the ride's total revenue
+  #   @rides.each do |ride|
+  #     ride.rider_log do |visitor, times|
+  #       @summary[visitor] = 3
+  #       @summary[revenue] = total_revenue 
+  #     end
+  #   end
+  #   @summary
+  # end
 end         
--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -85,4 +85,24 @@ RSpec.describe Carnival do
 
     expect(carnival.total_revenue).to eq(21)
   end
+
+  xit 'can provide a summary hash' do
+    carnival.add_ride(ride1)
+    carnival.add_ride(ride2)
+    carnival.add_ride(ride3)
+
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+    visitor2.add_preference(:thrilling)
+    visitor3.add_preference(:thrilling)
+
+    ride2.board_rider(visitor1)
+    ride2.board_rider(visitor2)
+    ride2.board_rider(visitor1)
+    ride3.board_rider(visitor3)
+    ride3.board_rider(visitor3)
+    ride3.board_rider(visitor3)
+
+    expect(carnival.summary_hash).to eq({:a => 1})
+  end
 end
