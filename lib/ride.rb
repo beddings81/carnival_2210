430066674efbbc4a1c82cfa18ac99ad183502170
Braxton Eddings <beddings81@gmail.com>
2022-11-14 11:09:54 -0600

    Finish iteration 2. spending money not decreasing

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,35 @@
 class Ride
+  attr_reader :name,
+              :min_height,
+              :admission_fee,
+              :excitement,
+              :total_revenue,
+              :rider_log
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @min_height = attributes[:min_height]
+    @admission_fee = attributes[:admission_fee]
+    @excitement = attributes[:excitement]
+    @total_revenue = 0
+    @rider_log = {}
+  end
+  
+  def board_rider(visitor)
+    # require 'pry'; binding.pry
+    if visitor.preferences.include?(@excitement) && visitor.height >= @min_height 
+      if @rider_log[visitor].nil?
+        rider_log[visitor] = 1
+        visitor.spending_money - @admission_fee
+        @total_revenue += @admission_fee
+      else
+        rider_log[visitor] += 1
+        visitor.spending_money - @admission_fee
+        @total_revenue += @admission_fee
+      end
+      # require 'pry'; binding.pry
+    end
+  end
+
 
 end
---
+++ spec/ride_spec.rb
@@ -0,0 +1,62 @@
+require './lib/visitor'
+require './lib/ride'
+
+RSpec.describe Ride do 
+  let!(:ride1) {Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })}
+  let!(:ride2) {Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })}
+  let!(:ride3) {Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })}
+  let!(:visitor1) {Visitor.new('Bruce', 54, '$10')}
+  let!(:visitor2) {Visitor.new('Tucker', 36, '$5')}
+  let!(:visitor3) {Visitor.new('Penny', 64, '$15')}
+
+  it 'exist and has attributes' do
+    expect(ride1).to be_a(Ride)
+    expect(ride1.name).to eq("Carousel")
+    expect(ride1.min_height).to eq(24)
+    expect(ride1.admission_fee).to eq(1)
+    expect(ride1.excitement).to eq(:gentle)
+    expect(ride1.total_revenue).to eq(0)
+  end
+
+  it 'can board riders and keep a log' do
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+
+    expect(ride1.rider_log).to eq({})
+
+    ride1.board_rider(visitor1)
+    ride1.board_rider(visitor2)
+    ride1.board_rider(visitor1)
+
+    expect(ride1.rider_log).to eq({visitor1 => 2, visitor2 => 1})
+    expect(ride1.total_revenue).to eq(3)
+  end
+
+  xit 'can take money from a visitor' do
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+
+    ride1.board_rider(visitor1)
+    ride1.board_rider(visitor2)
+    ride1.board_rider(visitor1)
+
+    expect(visitor1.spending_money).to eq(8)
+    expect(visitor2.spending_money).to eq(4)
+  end
+
+  it 'can board riders if they are tall enough or they have a matching preference' do
+    visitor2.add_preference(:thrilling)
+    visitor3.add_preference(:thrilling)
+
+    ride3.board_rider(visitor1)
+    ride3.board_rider(visitor2)
+    ride3.board_rider(visitor3)
+
+    expect(ride3.rider_log).to eq({visitor3 => 1})
+    # expect(visitor1.spending_money).to eq(8)
+    # expect(visitor2.spending_money).to eq(4)
+    # expect(visitor3.spending_money).to eq(13)
+    expect(ride3.total_revenue).to eq(2)
+  end
+
+end
